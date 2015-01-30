a5ee8e2bd81d826a1dbc895fb401b2a7d64283df
kshen4 <kshen4@me.com>
2015-01-30 02:16:07 -0500

    one more time

--- R/try001.R
+++ R/try001.R
@@ -1,16 +1,18 @@
 try001 <- function(n){
-    if(!require(rPython, character.only = TRUE)){
-        install.packages(rPython, dep = TRUE, repos = "http://star-www.st-andrews.ac.uk/cran/")
-        if(!require(rPython,character.only = TRUE)){
+    if(!require(rPython)){
+        install.packages("rPython", dep = TRUE, repos = "http://star-www.st-andrews.ac.uk/cran/")
+        if(!require(rPython)){
             stop("Package not found")
         }
     }
-    if(!require(RJSONIO, character.only = TRUE)){
-        install.packages(RJSONIO, dep = TRUE, repos = "http://star-www.st-andrews.ac.uk/cran/")
+    if(!require(RJSONIO)){
+        install.packages("RJSONIO", dep = TRUE, repos = "http://star-www.st-andrews.ac.uk/cran/")
         if(!require(RJSONIO,character.only = TRUE)){
             stop("Package not found")
         }
     }
+    library(RJSONIO)
+    library(rPython)
     path <- paste(system.file(package="compete"), "test.py", sep="/")
     python.load(path)
     k = python.call("test",n)
