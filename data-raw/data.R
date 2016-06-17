a6850a32f3b90f1f97a8e10cf1de17a74e7ae669
James Curley <jc3181@columbia.edu>
2016-06-17 09:50:19 -0400

    update data

--- data-raw/data.R
+++ data-raw/data.R
@@ -4,4 +4,8 @@ bonobos <- read.csv("data-raw/bonobos.csv",
                   stringsAsFactors = FALSE)
 people <- read.csv("data-raw/people.csv",
                     stringsAsFactors = FALSE)
-devtools::use_data(mouse, bonobos, people, overwrite = TRUE)
+caribou <- read.csv("data-raw/caribou.csv",
+                   stringsAsFactors = FALSE)
+el <- read.csv("data-raw/el.csv",
+                   stringsAsFactors = FALSE)
+devtools::use_data(mouse, bonobos, people, caribou,el, overwrite = TRUE)
